545b8627ea58b03e6935666cea98d186ee5f942b
Joachim Breitner <mail@joachim-breitner.de>
2009-09-16 14:45:54 +0000

    Read _NET_CLIENT_LIST for the list of applications
    Previous behaviour was dependant on the window manager’s reparenting behaviour.
    This now works at least with xmonad and metacity.

--- src/Capture.hs
+++ src/Capture.hs
@@ -14,9 +14,11 @@ captureData = do
 	dpy <- openDisplay ":0"
         xSetErrorHandler
 	let rwin = defaultRootWindow dpy
-	(_,_,cwins) <- queryTree dpy rwin
 
-	cwins <- filterM (isInterestingWindow dpy) cwins
+	a <- internAtom dpy "_NET_CLIENT_LIST" False
+	p <- getWindowProperty32 dpy a rwin
+	let cwins = maybe [] (map fromIntegral) p
+
 	(fwin,_) <- getInputFocus dpy
 	winData <- mapM (\w -> (,,) (w == fwin) <$> getWindowTitle dpy w <*> getProgramName dpy w) cwins
 
@@ -24,19 +26,6 @@ captureData = do
 
 	closeDisplay dpy
 	return $ CaptureData winData it
-
--- copied from XMonad/Main.hs, where function "ok" from "scan"
-isInterestingWindow :: Display -> Window -> IO Bool
-isInterestingWindow dpy w = do
-	wa <- getWindowAttributes dpy w
-	a  <- internAtom dpy "WM_STATE" False
-	p  <- getWindowProperty32 dpy a w
-	let ic = case p of
-		  Just (3:_) -> True -- 3 for iconified
-		  _          -> False
-	return $ not (wa_override_redirect wa)
-		 && (wa_map_state wa == waIsViewable || ic)
-
 
 getWindowTitle :: Display -> Window -> IO String
 getWindowTitle dpy w = fmap (fromMaybe "") $ fetchName dpy w
