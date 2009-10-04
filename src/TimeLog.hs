6636ca46707f193bd9ffe4378f32706a2a01eb40
Joachim Breitner <mail@joachim-breitner.de>
2009-10-04 10:54:28 +0000

    Switch to binary log file format

--- src/TimeLog.hs
+++ src/TimeLog.hs
@@ -1,25 +1,56 @@
 module TimeLog where
 
+import Data
+
 import Control.Applicative
 import System.IO
-import Data
 import Control.Concurrent
 import Control.Monad
 import Data.Time
+import Data.Binary
+import Data.Binary.Get
+import Data.Function
+import Data.Char
+import System.Directory
+
+import qualified Data.ByteString.Lazy as BS
+
+magic = BS.pack $ map (fromIntegral.ord) $ "arbtt-timelog-v1\n"
 
 -- | Runs the given action each delay milliseconds and appends the TimeLog to the
 -- given file.
-runLogger :: (Show a) => FilePath -> Integer -> IO a -> IO ()
+runLogger :: Binary a => FilePath -> Integer -> IO a -> IO ()
 runLogger filename delay action = forever $ do
 	entry <- action
 	date <- getCurrentTime
+	createTimeLog False filename
 	appendTimeLog filename (TimeLogEntry date delay entry)
 	threadDelay (fromIntegral delay * 1000)
 	
+createTimeLog :: Bool -> FilePath -> IO ()
+createTimeLog force filename = do
+	ex <- doesFileExist filename
+	when (not ex || force) $ BS.writeFile filename magic
 
-appendTimeLog :: Show a => FilePath -> TimeLogEntry a -> IO ()
--- Double show to ensure it is one string on one line
-appendTimeLog filename tl = appendFile filename $ (++"\n") $ show $ show $ tl
+appendTimeLog :: Binary a => FilePath -> TimeLogEntry a -> IO ()
+appendTimeLog filename tle = BS.appendFile filename $ encode $ tle
 
-readTimeLog :: Read a => FilePath -> IO (TimeLog a)
-readTimeLog filename = (map (read.read) . lines) <$> (openFile filename ReadMode >>= hGetContents)
+writeTimeLog :: Binary a => FilePath -> TimeLog a -> IO ()
+writeTimeLog filename tl = do createTimeLog True filename
+			      mapM_ (appendTimeLog filename) tl
+
+readTimeLog :: Binary a => FilePath -> IO (TimeLog a)
+readTimeLog filename = do
+	content <- BS.readFile filename
+        return $ runGet start content
+  where start = do
+  		startString <- getLazyByteString (BS.length magic)
+		if startString == magic
+		 then go
+		 else error $
+		 	"Timelog starts with unknown marker " ++
+			show (map (chr.fromIntegral) (BS.unpack startString))
+        go = do v <- get
+		m <- isEmpty
+		if m then return [v]
+		     else (v :) <$> go
---
+++ src/UpgradeLog1.hs
@@ -0,0 +1,61 @@
+module UpgradeLog1 (upgradeLogFile1) where
+
+import qualified Data.ByteString.Char8 as BS
+import System.IO
+import Data.Time
+import Control.Applicative
+import Control.Monad
+import System.Directory
+
+import TimeLog (writeTimeLog)
+import qualified Data as D
+
+-- | A copy of the data definitions as they were on 2009-10-03, to be able to
+-- change them in the main code later, and still be able to read old log files.
+
+type TimeLog a = [TimeLogEntry a]
+
+data TimeLogEntry a = TimeLogEntry
+	{ tlTime :: UTCTime
+	, tlRate :: Integer -- ^ in milli-seconds
+	, tlData :: a }
+  deriving (Show, Read)
+
+instance Functor TimeLogEntry where
+	fmap f tl = tl { tlData = f (tlData tl) }
+
+data CaptureData = CaptureData
+	{ cWindows :: [ (Bool, String, String) ]
+		-- ^ Active window, window title, programm name
+	, cLastActivity :: Integer -- ^ in milli-seconds
+	}
+  deriving (Show, Read)
+
+readTimeLog :: Read a => FilePath -> IO (TimeLog a)
+readTimeLog filename = (map (read.read) . lines) <$> (openFile filename ReadMode >>= hGetContents)
+
+magicStart = BS.pack "\"TimeLogEntry"
+
+upgradeLogFile1 captureFile = do
+        ex <- doesFileExist captureFile
+        when ex $ do
+                h <- openFile captureFile ReadMode	
+                start <- BS.hGet h (BS.length magicStart)
+                hClose h
+                when (start == magicStart) $ do
+                        putStrLn $ "Detected old text file format. Creating backup at " ++
+                                    oldFile ++ " and converting to new format..."
+                        renameFile captureFile oldFile
+                        captures <- readTimeLog oldFile
+                        writeTimeLog captureFile (upgrade captures)
+                        putStrLn $ "done."
+
+ where oldFile = captureFile ++ ".old"
+
+upgrade :: TimeLog CaptureData -> D.TimeLog D.CaptureData
+upgrade = map $ \(TimeLogEntry a b c) -> D.TimeLogEntry a b (upgradeCD c)
+
+upgradeCD :: CaptureData -> D.CaptureData
+upgradeCD (CaptureData a b) = D.CaptureData a b
+
+
