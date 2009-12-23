a700ab19f45fce24ee7f3447ccd8bfa205fa6aef
Joachim Breitner <mail@joachim-breitner.de>
2009-12-23 20:05:15 +0000

    Prepare CPP and cabal magic for portability

--- src/Capture.hs
+++ src/Capture.hs
@@ -1,5 +1,15 @@
+{-# LANGUAGE CPP #-}
+
 module Capture (
+#ifdef WIN32
+	module Capture.Win32
+#else
 	module Capture.X11
+#endif
 	) where
 
+#ifdef WIN32
+import Capture.Win32
+#else
 import Capture.X11
+#endif
