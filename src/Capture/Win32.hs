c71c4e6d5e11cc93a71b3bacfd11398931527e1f
Joachim Breitner <mail@joachim-breitner.de>
2009-12-24 00:13:07 +0000

    Add Capture.Win32

---
+++ src/Capture/Win32.hs
@@ -0,0 +1,28 @@
+module Capture.Win32 where
+
+import Data
+import Control.Monad
+import Control.Exception (bracket)
+import Control.Applicative
+import Data.Maybe
+import Data.Time.Clock
+import System.IO
+
+import Graphics.Win32.Window.Extra
+
+setupCapture :: IO ()
+setupCapture = do
+	return ()
+
+captureData :: IO CaptureData
+captureData = do
+	titles <- fetchWindowTitles
+	foreground <- getForegroundWindow
+
+	let winData = map (
+		\(h,t,p) -> (h == foreground, t, p)
+		) titles
+
+	it <- fromIntegral `fmap` getIdleTime
+
+	return $ CaptureData winData it
