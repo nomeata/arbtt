54a766b0d040762d12862402ff00142ffcb8b7a7
Joachim Breitner <mail@joachim-breitner.de>
2010-03-21 20:51:17 +0000

    Use Data.MyText in Win32 code

--- src/Capture/Win32.hs
+++ src/Capture/Win32.hs
@@ -1,6 +1,7 @@
 module Capture.Win32 where
 
 import Data
+import qualified Data.MyText as T
 import Control.Monad
 import Control.Exception (bracket)
 import Control.Applicative
@@ -20,7 +21,7 @@ captureData = do
         foreground <- getForegroundWindow
 
         let winData = map (
-                \(h,t,p) -> (h == foreground, t, p)
+                \(h,t,p) -> (h == foreground, T.pack t, T.pack p)
                 ) titles
 
         it <- fromIntegral `fmap` getIdleTime
