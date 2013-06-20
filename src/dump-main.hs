2d028dd1eb870fd2f1b06cde627a75c139823959
Joachim Breitner <mail@joachim-breitner.de>
2013-06-20 14:20:48 +0000

    Introduce --format flag for arbtt-dump

--- src/dump-main.hs
+++ src/dump-main.hs
@@ -9,19 +9,23 @@ import qualified Data.Map as M
 import Data.Version (showVersion)
 import Data.Maybe
 import Control.Monad
+import Data.Char
 
 import TimeLog
 import Data
 import CommonStartup
+import DumpFormat
 
 import Paths_arbtt (version)
 
 data Options = Options
     { optLogFile :: String
+    , optFormat :: DumpFormat
     }
 
 defaultOptions dir = Options
     { optLogFile = dir </> "capture.log"
+    , optFormat = DFHuman
     }
 
 
@@ -45,6 +49,17 @@ options =
      , Option "f"      ["logfile"]
               (ReqArg (\arg opt -> return opt { optLogFile = arg }) "FILE")
                "use this file instead of ~/.arbtt/capture.log"
+     , Option "t"      ["format"]
+              (ReqArg (\arg opt ->
+                case map toLower arg of 
+                    "human"      -> return $ opt { optFormat = DFHuman }
+                    "show"       -> return $ opt { optFormat = DFShow }
+                    "prettyjson" -> return $ opt { optFormat = DFPrettyJSON }
+                    "json"       -> return $ opt { optFormat = DFJSON }
+                    _            -> do hPutStrLn stderr ("Invalid format \"" ++ arg ++ "\".")
+                                       hPutStr stderr (usageInfo header options)
+                                       exitFailure) "FORMAT")
+               "output format, one of Human (default), Show, PrettyJSON or JSON "
      ]
 
 main = do
