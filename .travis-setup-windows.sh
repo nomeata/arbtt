172844d663e8a7f67264c5cdf327168fd085d36a
Joachim Breitner <mail@joachim-breitner.de>
2016-02-09 12:10:14 +0000

    Try to use newere minghc

--- .travis-setup-windows.sh
+++ .travis-setup-windows.sh
@@ -4,7 +4,7 @@ set -xe
 sudo add-apt-repository -y ppa:pipelight/stable
 sudo apt-get update
 sudo apt-get install --install-recommends wine-staging wine-staging-compat xvfb
-wget https://github.com/fpco/minghc/releases/download/2015-10-15/minghc-7.10.2-i386.exe
+wget https://github.com/fpco/minghc/releases/download/2015-12-04/minghc-7.10.2-i386.exe
 wine minghc-7.10.2-i386.exe /S | grep -v Extracting
 wine cabal --version
 wine cabal update
