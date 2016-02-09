a3ea19d749166b0a954588ef69ef262c0d0ab98b
Joachim Breitner <mail@joachim-breitner.de>
2016-02-09 11:34:14 +0000

    Reduce log output in windows travis setup

--- .travis-setup-windows.sh
+++ .travis-setup-windows.sh
@@ -5,7 +5,7 @@ sudo add-apt-repository -y ppa:pipelight/stable
 sudo apt-get update
 sudo apt-get install --install-recommends wine-staging wine-staging-compat xvfb
 wget https://github.com/fpco/minghc/releases/download/2015-10-15/minghc-7.10.2-i386.exe
-wine minghc-7.10.2-i386.exe /S
+wine minghc-7.10.2-i386.exe /S | grep -v Extracting
 wine cabal --version
 wine cabal update
 wget http://sourceforge.net/projects/gnuwin32/files/pcre/7.0/pcre-7.0.exe/download -O pcre-7.0.exe
