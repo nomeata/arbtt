db2d12c3d9b15c5b72060f1048f54465267802de
Joachim Breitner <mail@joachim-breitner.de>
2017-12-30 17:38:14 +0100

    Include travis_retry in setup script

--- .travis-setup-linux.sh
+++ .travis-setup-linux.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
 set -xe
+
+# https://github.com/travis-ci/travis-build/blob/master/lib/travis/build/templates/header.sh
+travis_retry() {
+  local result=0
+  local count=1
+  while [ $count -le 3 ]; do
+    [ $result -ne 0 ] && {
+      echo -e "\n${ANSI_RED}The command \"$@\" failed. Retrying, $count of 3.${ANSI_RESET}\n" >&2
+    }
+    # ! { } ignores set -e, see https://stackoverflow.com/a/4073372
+    ! { "$@"; result=$?; }
+    [ $result -eq 0 ] && break
+    count=$(($count + 1))
+    sleep 1
+  done
+
+  [ $count -gt 3 ] && {
+    echo -e "\n${ANSI_RED}The command \"$@\" failed 3 times.${ANSI_RESET}\n" >&2
+  }
+
+  return $result
+}
+
 sudo add-apt-repository -y ppa:hvr/ghc
 travis_retry sudo apt-get update
 travis_retry sudo apt-get install cabal-install-$CABALVER ghc-$GHCVER # see note about happy/alex
