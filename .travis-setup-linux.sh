83b2de523e3a4583114873a4f20ea7e4d24c8469
Joachim Breitner <mail@joachim-breitner.de>
2018-08-11 18:45:13 -0400

    Try to use head.hackage.haskell.org

--- .travis-setup-linux.sh
+++ .travis-setup-linux.sh
@@ -30,5 +30,19 @@ travis_retry sudo apt-get install cabal-install-$CABALVER ghc-$GHCVER # see note
 travis_retry sudo apt-get install libxss-dev libx11-dev libxrandr-dev libxinerama-dev
 cabal --version
 echo "$(ghc --version) [$(ghc --print-project-git-commit-id 2> /dev/null || echo '?')]"
+rm -f ${HOME}/.cabal/config
+
 travis_retry cabal update
+
+echo 'repository head.hackage'                                                        >> ${HOME}/.cabal/config
+echo '   url: http://head.hackage.haskell.org/'                                       >> ${HOME}/.cabal/config
+echo '   secure: True'                                                                >> ${HOME}/.cabal/config
+echo '   root-keys: 07c59cb65787dedfaef5bd5f987ceb5f7e5ebf88b904bbd4c5cbdeb2ff71b740' >> ${HOME}/.cabal/config
+echo '              2e8555dde16ebd8df076f1a8ef13b8f14c66bad8eafefd7d9e37d0ed711821fb' >> ${HOME}/.cabal/config
+echo '              8f79fd2389ab2967354407ec852cbe73f2e8635793ac446d09461ffb99527f6e' >> ${HOME}/.cabal/config
+echo '   key-threshold: 3'                                                            >> ${HOME}/.cabal.config
+sed -i 's/-- allow-newer: .*/allow-newer: *:base/' ${HOME}/.cabal/config
+
+travis_retry cabal update
+
 cabal install --only-dependencies --enable-tests --enable-benchmarks --force-reinstalls
